8c957f87be8bb86db6f90c8bd9ac2a8bd6e771c3
Layton Berth <laytonbrth@gmail.com>
2024-06-05 15:03:56 +0200

    refactor: Update Home page with RightSidebar component and recent transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Adrian' };
+  const loggedIn = { firstName: 'Layton' };
 
   return (
     <section className="home">
@@ -21,7 +22,11 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   )
 }
