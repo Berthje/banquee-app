996d1506ff100d88409e9331e6775db2898be0c5
Layton Berth <laytonbrth@gmail.com>
2024-06-05 14:43:26 +0200

    feat: Add Sidebar component to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Layton', lastName: 'Berth' };
+
   return (
     <main>
-        SIDEBAR
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
